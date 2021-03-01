44165e2076e8c904e1a02011f007506685008f3c
ronalli <khakimov.nikita.v@gmail.com>
2021-03-01 21:54:49 +0200

    dialogs-select

---
+++ src/redux/selectors/dialogs-selectors.js
@@ -0,0 +1,11 @@
+export const getDialogsNameSelector = (state) => {
+	return state.dialogsPage.dialogsNames;
+}
+
+export const getMessagesNamesSelector = (state) => {
+	return state.dialogsPage.messagesNames;
+}
+
+// export const getNewMessageTextSelector = (state) => {
+// 	return state.dialogsPage.newMessageText;
+// }
